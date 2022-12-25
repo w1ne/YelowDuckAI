d540b377e66ecb56114d402cdff47861011caf13
Andrii Shylenko <andrii@localhost.localdomain>
2022-12-25 22:19:17 +0100

    nitial commit, some tests

---
+++ test.py
@@ -0,0 +1,48 @@
+import os
+import openai
+from flask import Flask,render_template,request
+
+
+app = Flask(__name__)
+
+
+openai.api_key = os.getenv("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")
+
+#Flask chat app
+
+
+def response():
+    response = openai.Completion.create(
+    model="code-davinci-002",
+    prompt="class Log:\n    def __init__(self, path):\n        dirname = os.path.dirname(path)\n        os.makedirs(dirname, exist_ok=True)\n        f = open(path, \"a+\")\n\n        # Check that the file is newline-terminated\n        size = os.path.getsize(path)\n        if size > 0:\n            f.seek(size - 1)\n            end = f.read(1)\n            if end != \"\\n\":\n                f.write(\"\\n\")\n        self.f = f\n        self.path = path\n\n    def log(self, event):\n        event[\"_event_id\"] = str(uuid.uuid4())\n        json.dump(event, self.f)\n        self.f.write(\"\\n\")\n\n    def state(self):\n        state = {\"complete\": set(), \"last\": None}\n        for line in open(self.path):\n            event = json.loads(line)\n            if event[\"type\"] == \"submit\" and event[\"success\"]:\n                state[\"complete\"].add(event[\"id\"])\n                state[\"last\"] = event\n        return state\n\n\"\"\"\nHere's what the above class is doing:\n1.",
+    temperature=0,
+    max_tokens=64,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=["\"\"\""]
+)
+
+@app.route('/')
+def sessions():
+    return render_template('session.html')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: '+ str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+ 
+ 
+app.run(host='localhost', port=5000)
